0aff3320a36118d8ead3dfd3d2049d238021b179
Bao Vuong <vqbao@lv5.org>
2016-09-16 22:20:06 -0400

    completed kart randomizer for now

--- js/randomkartwii.js
+++ js/randomkartwii.js
@@ -38,9 +38,32 @@ function Track(name, cup) {
     this.name = name;
     this.cup = cup;
 }
+// =====================
 
+// ==== Vehicle class ====
 function Vehicle(name, type, weight) {
     this.name = name;
     this.vehicleType = type;
     this.weight = weight;
+}
+// =======================
+
+
+// ==== functions ====
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function shuffle(array) {
+    for (var i=0; i<array.length-1; i++) {
+        var j = getRandomInt(0, i);
+        if (j != i) {
+            // swap
+            var temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
 }
